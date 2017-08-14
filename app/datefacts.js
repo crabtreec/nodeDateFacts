319fcdbbf2296ba41e274abcd4533276df32f801
Chad Crabtree <chadcrabtree@Chads-MacBook-Pro.local>
2017-08-14 18:53:07 -0400

    Final

--- app/datefacts.js
+++ app/datefacts.js
@@ -1,3 +1,26 @@
 const timeFacts = require('moment');
-console.log("It is " + timeFacts().format('dddd LL, LTS') + ".")
-console.log("It is the " + timeFacts().dayOfYear() + "th day of the year.")
+const chalk = require('chalk');
+let currentSeconds = timeFacts([2017, 8, 14]).isDST()
+
+console.log("It is " + (chalk.blue(timeFacts().format('dddd LL, LTS'))) + ".")
+console.log("It is the " + (chalk.magenta(timeFacts().dayOfYear() + "th")) + " day of the year.")
+
+let hours = timeFacts().hours();
+let minutes = timeFacts().minutes();
+let seconds = timeFacts().seconds();
+
+console.log("It is " + (chalk.cyan((hours*60*60) + (minutes*60) + (seconds))) + " seconds into the day.")
+
+if (currentSeconds === true){
+  console.log("It " + (chalk.green("is")) + " during Daylight Savings Time.")
+
+} else {(console.log("It is not during Daylight Savings Time."))
+
+}
+
+let year = timeFacts().year();
+let leapYear = timeFacts([year]).isLeapYear();
+
+if (leapYear === false){
+  console.log("It " + (chalk.red("is not") + " a leap year"))
+}
